e17e6c5b30ab5a1acb6a2f5a0908eac32119749a
Victor Su <victor.yuchen.su@gmail.com>
2022-10-25 17:43:38 -0400

    added automatic scroller to site

--- README.md
+++ README.md
@@ -5,4 +5,3 @@ The website was made using HTML, CSS, and Javascript
 
 Pixelated borders by Luke Bonaccorsi: https://pixelcorners.lukeb.co.uk/?radius=8&multiplier=3
 VHR font by Riciery Leal: https://www.dafont.com/vcr-osd-mono.font
-Gameplay footage by: Steven Su
--- scripts.js
+++ scripts.js
@@ -6,3 +6,27 @@ toggleThemeBut.addEventListener('click', function () {
     root.className = theme;
     document.querySelector('.theme-name').textContent = newTheme;
 });
+
+//auto-scroller scripts
+let counter = 1;
+setInterval(() => {
+  document.getElementById(`radio${counter}`).checked = true;
+  if (counter >= 3) {
+    counter = 1;
+  } else {
+    counter += 1;
+  }
+}, 5000);
+const radio1 = document.getElementById("radio1");
+radio1.onclick = function () {
+  counter = 1;
+};
+const radio2 = document.getElementById("radio2");
+radio2.onclick = function () {
+  counter = 2;
+};
+const radio3 = document.getElementById("radio3");
+radio3.onclick = function () {
+  counter = 3;
+};
+//auto-scroller scripts end
